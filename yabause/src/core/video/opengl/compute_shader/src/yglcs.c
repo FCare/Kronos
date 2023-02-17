478059bfffd19dc498f2c3d46a9d20abad925c57
Francois CARON <francois.caron@renault.com>
2023-02-17 10:09:28 +0100

    Prohibited values are really prohibited - Fix Quake loading screen

--- yabause/src/core/video/opengl/compute_shader/src/yglcs.c
+++ yabause/src/core/video/opengl/compute_shader/src/yglcs.c
@@ -78,13 +78,13 @@ int YglEraseWriteCSVDP1(int id) {
   limits[2] = (((Vdp1Regs->EWRR>>9)&0x7F)<<shift) - 1;
   limits[3] = ((Vdp1Regs->EWRR)&0x1FF); //TODO: manage double interlace
 
-  //Prohibited value seems to be considered as maximum values
-  if (limits[2] == -1) limits[2] = 511;
-  if (limits[3] == 0) limits[3] = 511;
+  //Prohibited value - Example Quake first screens
+  if ((limits[2] == -1)||(limits[3] == 0)) return;
 
   if ((limits[0]>=limits[2])||(limits[1]>limits[3])) {
     return 0; //No erase write when invalid area - Should be done only for one dot but no idea of which dot it shall be
   }
+
 
 //Can be usefull for next steps to evaluate effective possible pixels which can be deleted during VBLANK
 //see p49 of vdp1 doc. A raster is the number of maxLinecount
